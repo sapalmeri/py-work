c53e95d314afb7a755d4b7a409a2e63aa397fb78
Shane Palmeri <shanepalmeri@Shanes-MacBook-Pro.local>
2022-01-06 22:10:38 -0500

    finished lesson 8

---
+++ 8_error_handlingtodo.py
@@ -0,0 +1,33 @@
+"""
+Lets clean up some errors
+"""
+
+
+# TODO: Section 1.1:
+
+# Run the code below to find what type of error you are encountering. Then comment out the code and
+# rewrite it with a new denominator to fix the error. Then print the type of error you got from the
+# original problem.
+
+# number = 10 * (1/0) # FIXME
+# ZeroDivisionError: division by zero
+number = 10 * (1/2)
+print(ZeroDivisionError)
+
+
+# TODO: Section 1.2:
+# Again, run the code below to find what type of error you are encountering. Then define 'spam' on
+# the correct line and print the error type you originally received.
+
+# equation = 4 + spam * 3 # FIXME
+spam = 2
+equation = 4 + spam * 3
+print(NameError)
+
+# TODO: Section 1.3:
+# Finally, run the code below to find what type of error you are encountering. Change the code so that
+# our '2 + 2' addition works and print the error type you originally received.
+
+# twotwo = '2' + 2 # FIXME
+twotwo = 2 + 2
+print(TypeError)
